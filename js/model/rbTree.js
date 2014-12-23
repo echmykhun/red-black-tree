bf76a16d43f4de0aca8ea0f12c1ef356209662af
eugene.chmykhun <eugene.chmykhun@ws323.i.sigmaukraine.com>
2014-12-23 16:03:35 +0200

    + small start

--- js/model/rbTree.js
+++ js/model/rbTree.js
@@ -1,6 +1,48 @@
-var rbTree = function(){};
+var rbTree = function () {
+};
 
 rbTree.prototype = {
 
-    rbTree: function(){}
+    rbTree: function () {
+    },
+    root: new rbNode()
+};
+
+
+var rbNode = function () {
+};
+
+rbNode.prototype = {
+    id: -1,
+    tree: null,
+    left: null,
+    right: null,
+    parent: null,
+    isRed: false,
+    value: 0,
+    isNil: true,
+    rbNode: function () {
+    },
+    rotateLeft: function () {
+
+        var y = this.right;
+        this.right = y.left;
+        if (!y.left.isNil) {
+            y.left.parent = this;
+        }
+        if (!y.isNil) y.parent = this.parent;
+        if (this.parent) {
+            if (this.id == this.parent.left.id) {
+                this.parent.left = y;
+            }
+            else {
+                this.parent.right = y;
+            }
+        } else {
+            this.tree.root = y;
+        }
+
+        y.left = this;
+
+    }
 };
