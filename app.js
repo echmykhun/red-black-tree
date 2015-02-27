fe27ad4dd6aed92368771a30c89f8f603929b417
eugene.chmykhun <e.chmykhun@yandex.ru>
2015-02-28 01:20:22 +0200

    +small

--- app.js
+++ app.js
@@ -23,25 +23,17 @@ http.createServer(function (request, response) {
             break;
     }
 
-    path.exists(filePath, function(exists) {
-
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-        else {
-            response.writeHead(404);
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            response.writeHead(500);
             response.end();
         }
+        else {
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
     });
+
 
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
